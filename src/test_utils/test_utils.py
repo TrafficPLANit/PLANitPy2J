f5a9452f382f591ec1af830f25a22cba6691a401
Mark Raadsen <mark.raadsen@sydney.edu.au>
2020-06-04 14:14:40 +1000

    changed parameter order

--- src/test_utils/test_utils.py
+++ src/test_utils/test_utils.py
@@ -109,7 +109,7 @@ class PlanItHelper:
                 
         plan_it.assignment.physical_cost.set_default_parameters(0.8, 4.5, 1, 1)
         link_segment_external_id = 3
-        mode_external_id = 1
+        mode_external_id = 1 
         plan_it.assignment.physical_cost.set_parameters(1.0, 5.0, mode_external_id, link_segment_external_id)       
                 
         plan_it.run()
