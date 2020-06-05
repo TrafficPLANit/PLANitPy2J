45f478ee1440ce6570d212e289219bdf6c0acced
Gary Mann <g.mann@sydney.edu.au>
2020-06-05 22:46:53 +1000

    PLANitIO#19: Relative path to CSV file included in XML output

--- src/planit/gateway.py
+++ src/planit/gateway.py
@@ -9,16 +9,16 @@ class GatewayConfig(object):
     # Currently we provide the paths for the local environment AND production environment
     # TODO there should be a mechanism such that we only need one type of definition
     
-    RELEASE_SHARE_PATH = os.path.join(os.path.split(sys.executable)[0],'share')
-    IDE_SHARE_PATH = os.path.join('..','..','share')
+    RELEASE_SHARE_PATH = os.path.join(os.path.split(sys.executable)[0], 'share')
+    IDE_SHARE_PATH = os.path.join('..', '..', 'share')
     # VENV (virtual environment) for Python erroneously results in sys.executable pointing NOT to the root
     # directory of the virtual environment, but to ./Scripts. Hence, we must account for that by alos looking
     # one directory upward.
     # This (as far as I know) is a bug in venv!
-    VENV_RELEASE_SHARE_PATH = os.path.join(os.path.split(sys.executable)[0],'..','share')
+    VENV_RELEASE_SHARE_PATH = os.path.join(os.path.split(sys.executable)[0], '..', 'share')
     
-    PLANIT_SHARE = os.path.join('planit','*')
-    PY4J_SHARE = os.path.join('py4j','*')
+    PLANIT_SHARE = os.path.join('planit', '*')
+    PY4J_SHARE = os.path.join('py4j', '*')
     
     # the main entry point of the Java gateway implementation for PLANit
     JAVA_GATEWAY_WRAPPER_CLASS =  'org.planit.python.PLANitJ2Py'
